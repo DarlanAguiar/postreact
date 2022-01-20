34391a83cca365e9ef340ccb9b19f14b1b9a956f
DarlanAguiar <darlangaguiar@gmail.com>
2022-01-20 11:29:36 -0400

    removendo a validação de data vazia

--- src/components/CardPostit.jsx
+++ src/components/CardPostit.jsx
@@ -16,6 +16,12 @@ const CardPostit = ({ info, deletePost, editPost }) => {
   };
 
   const validateDate = (date) => {
+
+    if(date.length < 1){
+      setInvalidDate(false)
+      return
+    }
+    
     let expressao = new RegExp(
       /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
       "g"
@@ -30,9 +36,7 @@ const CardPostit = ({ info, deletePost, editPost }) => {
     }
   };
 
-  if (info.editDate) {
-    console.log(info.editDate);
-  }
+
 
   return (
     <div className="card">
